3dfe44213da3824371616973d4f508f75a01ff47
SleepySpeller <janjicstefan2008@gmail.com>
2022-02-01 22:02:20 +0100

    Add files via upload

---
+++ client.py
@@ -0,0 +1,30 @@
+import socket
+import threading
+
+nickname = input("Choose a nickname: ")
+
+client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+client.connect(('127.0.0.1', 45399))
+
+def recive():
+    while True:
+        try:
+            message = client.recv(1024).decode('ascii')
+            if message == 'NICK':
+                client.send(nickname.encode('ascii'))
+            else:
+                print(message)
+        except:
+            print("An error occurred!")
+            client.close()
+            break
+
+def write():
+    while True:
+        message = f'{nickname}: {input("")}'
+        client.send(message.encode('ascii'))
+recive_thread = threading.Thread(target=recive)
+recive_thread.start()
+
+write_thread = threading.Thread(target=write)
+write_thread.start()
