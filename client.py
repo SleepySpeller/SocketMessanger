5ae1d991914b1d3ab1a32d03f8c95e5bc8d6e17b
SleepySpeller <janjicstefan2008@gmail.com>
2022-02-02 12:28:10 +0100

    Added passwords

--- client.py
+++ client.py
@@ -2,6 +2,7 @@ import socket
 import threading
 
 nickname = input("Choose a nickname: ")
+password = input("Password: ")
 
 client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 client.connect(('127.0.0.1', 45399))
@@ -12,6 +13,18 @@ def recive():
             message = client.recv(1024).decode('ascii')
             if message == 'NICK':
                 client.send(nickname.encode('ascii'))
+            elif message == 'PASS':
+                client.send(password.encode('ascii'))
+            elif message == 'ERR_PASS_INC':
+                print(f"Incorrect password! Error code {message}")
+                break
+            elif message == 'ADMIN_PASS':
+                adminPassword = input(f"The server has requested to enter the password of your account ({nickname}): ")
+                client.send(adminPassword.encode('ascii'))
+            elif message == 'ERR_ADMIN_INC':
+                print(f"User account password is incorrect, disconnecting... ")
+                client.close()
+                print("Disconnected. Error code: ERR_ADMIN_INC")
             else:
                 print(message)
         except:
--- server.py
+++ server.py
@@ -1,8 +1,17 @@
+from re import I
 import threading
 import socket
 
+inputPassword = ""
+inputPasswordAdmin = ""
+success = True
+
 host = "127.0.0.1"
 port = 45399
+password = "abc"
+
+admins = ["Speller"]
+adminPasswords = ["def"]
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 server.bind((host, port))
@@ -31,22 +40,72 @@ def handle(client):
             nicknames.remove(nickname)
             break
 
+def successfulLogin(nickname, client):
+    nicknames.append(nickname)
+    clients.append(client)
+
+    print(f"Nickname of the client is {nickname}!")
+    broadcast(f'{nickname} joined the chat!'.encode('ascii'))
+    client.send('Connected to the server!'.encode('ascii'))
+
+    thread = threading.Thread(target=handle, args=(client, ))
+    thread.start()
+
 def recive():
     while True:
         client, address = server.accept()
         print(f"Connected with {str(address)}")
 
+        success = True
+        ## Requesting the client to send the username
         client.send('NICK'.encode('ascii'))
         nickname = client.recv(1024).decode('ascii')
-        nicknames.append(nickname)
-        clients.append(client)
 
-        print(f"Nickname of the client is {nickname}!")
-        broadcast(f'{nickname} joined the chat!'.encode('ascii'))
-        client.send('Connected to the server!'.encode('ascii'))
+        ## Requesting the client to send the password for the server
+        client.send('PASS'.encode('ascii'))
+        inputPassword = client.recv(1024).decode('ascii')
 
-        thread = threading.Thread(target=handle, args=(client, ))
-        thread.start()
+        ## Checking if the password is correct
+        if inputPassword == password:
+            pass
+        else:
+            client.send('ERR_PASS_INC'.encode('ascii'))
+            client.close()
+            success = False
+            break
+
+        for i in range(len(admins)):
+            if str(admins[i]) == str(nickname):
+                client.send('ADMIN_PASS'.encode('ascii'))
+                inputPasswordAdmin = client.recv(1024).decode('ascii')
+                
+                if inputPasswordAdmin == adminPasswords[i]:
+                    pass
+                else:
+                    client.send('ERR_ADMIN_INC'.encode('ascii'))
+                    client.close()
+                    success = False
+                    break
+        if success == True:
+            try:
+                nicknames.append(nickname)
+                clients.append(client)
+
+                print(f"Nickname of the client is {nickname}!")
+                broadcast(f'{nickname} joined the chat!'.encode('ascii'))
+                client.send('Connected to the server!'.encode('ascii'))
+
+                thread = threading.Thread(target=handle, args=(client, ))
+                thread.start()
+            except:
+                print("Error!")
+                #client.send('ERR_SOCK_LOG')
+                #client.close
+        
+        
 
 print("Server is listening!")
 recive()
+
+if __name__ == "__main__":
+    recive()  
